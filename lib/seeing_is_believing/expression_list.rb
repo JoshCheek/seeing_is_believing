7302dd0832e00673428d898c95c21081931051fa
Josh Cheek <josh.cheek@gmail.com>
2012-12-31 19:23:14 -0600

    Refactah ExpressionList

--- lib/seeing_is_believing/expression_list.rb
+++ lib/seeing_is_believing/expression_list.rb
@@ -13,38 +13,25 @@ class SeeingIsBelieving
     end
 
     def push(expression, options)
-      puts "ADDING: #{expression.inspect} TO: #{@list.inspect}"
       @line_number += 1
       @list << PendingExpression.new(expression, options[:on_complete], [])
       reduce_expressions options[:generate]
     end
 
-    def empty?
-      @list.empty?
-    end
-
     private
 
-    # :(
+    # O.o
     def reduce_expressions(generate)
       expressions = @list.map(&:expression)
       @list.size.times do |i|
         expression = expressions.join "\n" # must use newline otherwise can get expressions like `a\\+b` that should be `a\\\n+b`, former is invalid
-        unless valid_ruby? expression
-          expressions.shift
-          next
-        end
+        next expressions.shift unless valid_ruby? expression
         result = @list[i].on_complete.call(@list[i].expression,
                                            @list[i].children,
                                            @list[i+1..-1].map(&:expression),
                                            @line_number)
-        if i.zero?
-          @list = []
-        else
-          @list = @list[0..i-1]
-        end
+        @list = @list[0, i]
         @list[i-1].children << result unless @list.empty?
-        puts "RESULT: #{result.inspect}, #{@list.inspect}"
         return result
       end
       generate.call
@@ -55,31 +42,3 @@ class SeeingIsBelieving
     end
   end
 end
-
-
-#     @line_number += 1
-
-#     if previous_expressions.empty? && valid_ruby?(line)
-#       record_yahself line, @line_number
-#     elsif previous_expressions.empty?
-#       next_expression = get_next_expression [line]
-#       record_yahself next_expression, @line_number
-#     elsif completes_previous_expression?(previous_expressions, line)
-#       previous_expression previous_expressions, line
-#     end
-#   end
-
-#   def self.previous_expression(previous_expressions,
-
-#   def any_valid?(previous_expressions, current_expression)
-#     puts "PREVIOUS EXPRESSIONS: #{previous_expressions.inspect}"
-#     expression = current_expression
-#     index = previous_expressions.size - 1
-#     until index == -1
-#       expression = previous_expressions[index] + expression
-#       return true if valid_ruby? expression
-#       index -= 1
-#     end
-#     false
-#   end
-
--- spec/expression_list_spec.rb
+++ spec/expression_list_spec.rb
@@ -3,47 +3,46 @@ require 'seeing_is_believing/expression_list'
 describe SeeingIsBelieving::ExpressionList do
   subject :list
 
-  # it 'evaluates the on_complete if it is complete' do
-  #   var = nil
-  #   list.push('a', on_complete: Proc.new { var = 1 }).should == 1
-  #   var.should == 1
-  # end
+  it 'evaluates the on_complete if it is complete' do
+    var = nil
+    list.push('a', on_complete: Proc.new { var = 1 }).should == 1
+    var.should == 1
+  end
 
-  # it 'evaluates the on_incomplete if it is incomplete' do
-  #   var = nil
-  #   list.push('a+', on_incomplete: Proc.new { var = 2 }).should == 2
-  #   var.should == 2
-  # end
+  it 'evaluates generate if it is incomplete' do
+    var = nil
+    list.push('a+', generate: lambda { var = 2 }).should == 2
+    var.should == 2
+  end
 
-  # it 'passes the line and line number to the callbacks' do
-  #   described_class.new.push 'a', on_complete: Proc.new { |line, line_number|
-  #     line.should == 'a'
-  #     line_number.should == 1
-  #   }
-  #   described_class.new.push 'a+', on_incomplete: Proc.new { |line, line_number|
-  #     line.should == 'a+'
-  #     line_number.should == 1
-  #   }
-  # end
+  it 'passes the line, children, expressions that complete the current expression, and line number to the on_complete callback' do
+    described_class.new.push 'a', on_complete: lambda { |line, children, completions, line_number|
+      line.should == 'a'
+      line_number.should == 1
+    }
+  end
 
-  # it 'increments the line number for each expression added' do
-  #   i = 0
-  #   list.push 'a',  on_complete: Proc.new { |_, num| num.should == 1; i += 1 }
-  #   list.push 'b+', on_incomplete: Proc.new { |_, num| num.should == 2; i += 10 }
-  #   i.should == 11
-  # end
+  it 'increments the line number for each expression added' do
+    i = 0
+    list.push 'a',  on_complete: lambda { |_, _, _, num| num.should == 1; i += 1   }
+    list.push 'b+', on_complete: lambda { |_, _, _, num| num.should == 3; i += 10  }, generate: lambda {}
+    list.push 'a',  on_complete: lambda { |_, _, _, num| num.should == 3; i += 100 }
+    i.should == 11
+  end
 
-  it '???' do
-    noops = {on_complete: -> line, children, completions, line_number {
-      p [line, children, completions, line_number]
-      line+children.join(' ! ')+completions.join('') }, generate: Proc.new {}}
-    list.push('a', noops).should == 'a'
-    list.push 'a(', noops
-    list.push 'b+', noops
-    list.push('c', noops).should == 'b+c'
-    list.push 'x\\', noops
-    list.push '+', noops
-    list.push('y', noops).should == 'x\\+y'
-    list.push(')', noops).should == 'a(b+c ! x\\+y)'
+  example 'a sophisticated example' do
+    callbacks = { generate: Proc.new {},
+                  on_complete: -> line, children, completions, line_number do
+                    line+children.join(' ! ')+completions.join('')
+                  end
+                }
+    list.push('a',   callbacks).should == 'a'
+    list.push('a(',  callbacks)
+    list.push('b+',  callbacks)
+    list.push('c',   callbacks).should == 'b+c'
+    list.push('x\\', callbacks)
+    list.push('+',   callbacks)
+    list.push('y',   callbacks).should == 'x\\+y'
+    list.push(')',   callbacks).should == 'a(b+c ! x\\+y)'
   end
 end
