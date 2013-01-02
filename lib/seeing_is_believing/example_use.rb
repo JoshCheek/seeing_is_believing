221ef6a50e9421d17e2e6862cf782282cf64a3c1
Josh Cheek <josh.cheek@gmail.com>
2013-01-01 21:17:45 -0600

    Remove binary as a first class thing, instead have an example use obj
    and put the responsibility of args and streams into the actual bin file

---
+++ lib/seeing_is_believing/example_use.rb
@@ -0,0 +1,40 @@
+require 'seeing_is_believing'
+
+class SeeingIsBelieving
+  class ExampleUse
+    def initialize(body)
+      self.body = body
+    end
+
+    def call
+      body.each_line.with_index 1 do |line, index|
+        write_line line.chomp, results[index], line_length
+      end
+      output
+    end
+
+    private
+
+    attr_accessor :body
+
+    def results
+      @results ||= SeeingIsBelieving.new(body).call
+    end
+
+    def output
+      @result ||= ''
+    end
+
+    def write_line(line, results, line_length)
+      if results.any?
+        output << sprintf("%-#{line_length}s# => %s\n", line, results.join(', '))
+      else
+        output << line << "\n"
+      end
+    end
+
+    def line_length
+      @line_length ||= body.each_line.map(&:chomp).map(&:length).max + 2
+    end
+  end
+end
