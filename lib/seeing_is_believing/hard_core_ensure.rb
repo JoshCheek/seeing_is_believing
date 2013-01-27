c171200516294f252be55663b2d0994a90f664ee
Josh Cheek <josh.cheek@gmail.com>
2013-01-26 19:07:19 -0600

    Write the HardCoreEnsure module...
    Why is this not a thing in Ruby already?
    
    Am I missing any other important interrupts?
    
    I know it wont work right for sigkill, but I don't think there's any way
    around that.

---
+++ lib/seeing_is_believing/hard_core_ensure.rb
@@ -0,0 +1,48 @@
+class SeeingIsBelieving
+  class HardCoreEnsure
+    def initialize(options)
+      self.options = options
+      validate_options
+    end
+
+    def call
+      trap_sigint
+      invoke_code
+    ensure
+      invoke_ensure
+    end
+
+    private
+
+    attr_accessor :options, :ensure_invoked, :old_handler
+
+    def trap_sigint
+      self.old_handler = trap 'INT' do
+        invoke_ensure
+        Process.kill 'INT', $$
+      end
+    end
+
+    def invoke_code
+      options[:code].call
+    end
+
+    def invoke_ensure
+      return if ensure_invoked
+      trap 'INT', old_handler
+      self.ensure_invoked = true
+      options[:ensure].call
+    end
+
+    def validate_options
+      raise ArgumentError, "Must pass the :code key"   unless options.key? :code
+      raise ArgumentError, "Must pass the :ensure key" unless options.key? :ensure
+      unknown_keys = options.keys - [:code, :ensure]
+      if options.size == 3
+        raise ArgumentError, "Unknown key: #{unknown_keys.first.inspect}"
+      elsif options.size > 3
+        raise ArgumentError, "Unknown keys: #{unknown_keys.map(&:inspect).join(', ')}"
+      end
+    end
+  end
+end
---
+++ spec/hard_core_ensure_spec.rb
@@ -0,0 +1,73 @@
+require 'ichannel'
+require 'seeing_is_believing/hard_core_ensure'
+
+describe SeeingIsBelieving::HardCoreEnsure do
+  def call(options)
+    described_class.new(options).call
+  end
+
+  it "raises an argument error if it doesn't get a code proc" do
+    expect { call ensure: -> {} }.to raise_error ArgumentError, "Must pass the :code key"
+  end
+
+  it "raises an argument error if it doesn't get an ensure proc" do
+    expect { call code: -> {} }.to raise_error ArgumentError, "Must pass the :ensure key"
+  end
+
+  it "raises an argument error if it gets any other keys" do
+    expect { call code: -> {}, ensure: -> {}, other: 123 }.to \
+      raise_error ArgumentError, "Unknown key: :other"
+
+    expect { call code: -> {}, ensure: -> {}, other1: 123, other2: 456 }.to \
+      raise_error ArgumentError, "Unknown keys: :other1, :other2"
+  end
+
+  it 'invokes the code and returns the value' do
+    call(code: -> { :result }, ensure: -> {}).should == :result
+  end
+
+  it 'invokes the ensure after the code' do
+    seen = []
+    call code: -> { seen << :code }, ensure: -> { seen << :ensure }
+    seen.should == [:code, :ensure]
+  end
+
+  it 'invokes the ensure even if an exception is raised' do
+    ensure_invoked = false
+    expect do
+      call code: -> { raise Exception, 'omg!' }, ensure: -> { ensure_invoked = true }
+    end.to raise_error Exception, 'omg!'
+    ensure_invoked.should == true
+  end
+
+  it 'invokes the code even if an interrupt is sent and there is a default handler' do
+    channel = IChannel.new Marshal
+    pid = fork do
+      old_handler = trap('INT') { channel.put "old handler invoked" }
+      call code: -> { sleep 0.1 }, ensure: -> { channel.put "ensure invoked" }
+      trap 'INT', old_handler
+    end
+    sleep 0.05
+    Process.kill 'INT', pid
+    Process.wait pid
+    channel.get.should == "ensure invoked"
+    channel.get.should == "old handler invoked"
+    channel.should_not be_readable
+  end
+
+  it 'invokes the code even if an interrupt is sent and interrupts are set to ignore' do
+    channel = IChannel.new Marshal
+    pid = fork do
+      old_handler = trap 'INT', 'IGNORE'
+      result = call code: -> { sleep 0.1; 'code result' }, ensure: -> { channel.put "ensure invoked" }
+      channel.put result
+      trap 'INT', old_handler
+    end
+    sleep 0.05
+    Process.kill 'INT', pid
+    Process.wait pid
+    channel.get.should == "ensure invoked"
+    channel.get.should == 'code result'
+    channel.should_not be_readable
+  end
+end
