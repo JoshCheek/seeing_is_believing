766c1752ac93a76600e02471d3b4528810731f27
Josh Cheek <josh.cheek@gmail.com>
2013-02-16 14:07:44 -0600

    Syntactic refactoring of step definitions

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,35 +1,9 @@
-Given 'the file "$filename":' do |filename, body|
-  CommandLineHelpers.write_file filename, body
-end
-
-Given 'the file "$filename" "$body"' do |filename, body|
-  CommandLineHelpers.write_file filename, body
-end
-
-Given 'I have the stdin content "$content"' do |content|
-  @stdin_data = content
-end
-
-When 'I run "$command"' do |command|
-  @last_executed = CommandLineHelpers.execute command, @stdin_data
-end
-
-Then /^(stderr|stdout) is:$/ do |stream_name, output|
-  @last_executed.send(stream_name).chomp.should == eval_curlies(output)
-end
-
-Then /^(stderr|stdout) is "(.*?)"$/ do |stream_name, output|
-  @last_executed.send(stream_name).chomp.should == eval_curlies(output)
-end
-
-Then 'the exit status is $status' do |status|
-  @last_executed.exitstatus.to_s.should == status
-end
-
-Then /^(stderr|stdout) is empty$/ do |stream_name|
-  @last_executed.send(stream_name).should == ''
-end
-
-Then /^(stderr|stdout) includes "([^"]*)"$/ do |stream_name, content|
-  @last_executed.send(stream_name).should include content
-end
+Given('the file "$filename" "$body"')        { |filename, body|       CommandLineHelpers.write_file filename, body }
+Given('the file "$filename":')               { |filename, body|       CommandLineHelpers.write_file filename, body }
+Given('I have the stdin content "$content"') { |content|              @stdin_data = content }
+When('I run "$command"')                     { |command|              @last_executed = CommandLineHelpers.execute command, @stdin_data }
+Then(/^(stderr|stdout) is:$/)                { |stream_name, output|  @last_executed.send(stream_name).chomp.should == output }
+Then(/^(stderr|stdout) is "(.*?)"$/)         { |stream_name, output|  @last_executed.send(stream_name).chomp.should == output }
+Then(/^(stderr|stdout) is empty$/)           { |stream_name|          @last_executed.send(stream_name).should == '' }
+Then(/^(stderr|stdout) includes "([^"]*)"$/) { |stream_name, content| @last_executed.send(stream_name).should include content }
+Then('the exit status is $status')           { |status|               @last_executed.exitstatus.to_s.should == status }
