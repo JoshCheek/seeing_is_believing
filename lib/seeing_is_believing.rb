f1b5928619953e4018374e29bc203ca0de9ef847
Josh Cheek <josh.cheek@gmail.com>
2013-08-06 18:13:29 -0500

    Delete tons of code that is no longer used since we added ProgramRewriter

--- lib/seeing_is_believing.rb
+++ lib/seeing_is_believing.rb
@@ -21,15 +21,13 @@ class SeeingIsBelieving
   end
 
   def initialize(program, options={})
-    @_program         = program
-    @stream          = to_stream program
+    @program         = program
     @matrix_filename = options[:matrix_filename]
     @filename        = options[:filename]
     @stdin           = to_stream options.fetch(:stdin, '')
     @require         = options.fetch :require, []
     @load_path       = options.fetch :load_path, []
     @encoding        = options.fetch :encoding, nil
-    @line_number     = 1
     @timeout         = options[:timeout]
     @debugger        = options.fetch :debugger, Debugger.new(enabled: false)
   end
@@ -37,7 +35,7 @@ class SeeingIsBelieving
   def call
     @memoized_result ||= begin
       # must use newline after code, or comments will comment out rescue section
-      wrapped = ProgramReWriter.call "#@_program\n",
+      wrapped = ProgramReWriter.call "#@program\n",
                                      before_all:  "begin;",
                                      after_all:   "\n"\
                                                   "rescue Exception;"\
@@ -54,85 +52,13 @@ class SeeingIsBelieving
   end
 
 
-#       leading_comments = ''
-
-
-#       # extract leading comments (e.g. encoding) so they don't get wrapped in begin/rescue/end
-#       while SyntaxAnalyzer.line_is_comment?(next_line_queue.peek)
-#         leading_comments << next_line_queue.dequeue << "\n"
-#         @line_number += 1
-#       end
-
-#       # extract leading =begin/=end so they don't get wrapped in begin/rescue/end
-#       while SyntaxAnalyzer.begins_multiline_comment?(next_line_queue.peek)
-#         lines = next_line_queue.dequeue << "\n"
-#         @line_number += 1
-#         until SyntaxAnalyzer.begin_and_end_comments_are_complete? lines
-#           lines << next_line_queue.dequeue << "\n"
-#           @line_number += 1
-#         end
-#         leading_comments << lines
-#       end
-
-#       # extract program body
-#       body = ''
-#       until next_line_queue.empty? || data_segment?
-#         expression, expression_size = expression_list.call
-#         body << expression
-#         track_line_number @line_number
-#         @line_number += expression_size
-#       end
-
-#       # extract data segment
-#       data_segment = ''
-#       data_segment = "\n#{the_rest_of_the_stream}" if data_segment?
-
-#       # build the program
-#       program = leading_comments << record_exceptions_in(body) << data_segment
-
-#       # return the result
-#       result_for program, max_line_number
-#     end
-#   end
-
   private
 
-  attr_reader :stream, :matrix_filename, :debugger
-
-  def expression_list
-    @expression_list ||= ExpressionList.new debugger:       debugger,
-                                            get_next_line:  lambda { next_line_queue.dequeue },
-                                            peek_next_line: lambda { next_line_queue.peek },
-                                            on_complete:    lambda { |line, children, completions, offset|
-                                              expression = [line, *children, *completions].map(&:chomp).join("\n")
-
-                                              if do_not_record? expression
-                                                expression + "\n"
-                                              else
-                                                record_yahself(expression, @line_number+offset) + "\n"
-                                              end
-                                            }
-  end
+  attr_reader :matrix_filename, :debugger
 
   def to_stream(string_or_stream)
     return string_or_stream if string_or_stream.respond_to? :gets
     StringIO.new string_or_stream
-  end
-
-  def record_yahself(line, line_number)
-    "($seeing_is_believing_current_result.record_result(#{line_number}, (#{line})))"
-  end
-
-  def record_exceptions_in(code)
-    # must use newline after code, or comments will comment out rescue section
-    "begin;"\
-      "#{code}\n"\
-    "rescue Exception;"\
-      "line_number = $!.backtrace.grep(/\#{__FILE__}/).first[/:\\d+/][1..-1].to_i;"\
-      "$seeing_is_believing_current_result.record_exception line_number, $!;"\
-      "$seeing_is_believing_current_result.exitstatus = 1;"\
-      "$seeing_is_believing_current_result.exitstatus = $!.status if $!.kind_of? SystemExit;"\
-    "end"
   end
 
   def result_for(program)
@@ -148,29 +74,5 @@ class SeeingIsBelieving
                                 timeout:         @timeout)
                            .call
     end
-  end
-
-  def eof?
-    next_line_queue.peek.nil?
-  end
-
-  def data_segment?
-    SyntaxAnalyzer.begins_data_segment?(next_line_queue.peek)
-  end
-
-  def next_line_queue
-    @next_line_queue ||= Queue.new { (line = stream.gets) && line.chomp }
-  end
-
-  # eh? -.^ (can't we pull the rest of this out of the queue instead of breaking encapsulation?)
-  def the_rest_of_the_stream
-    next_line_queue.dequeue << "\n" << stream.read
-  end
-
-  def do_not_record?(code)
-    code =~ BLANK_REGEX                           ||
-      SyntaxAnalyzer.ends_in_comment?(code)       ||
-      SyntaxAnalyzer.void_value_expression?(code) ||
-      SyntaxAnalyzer.here_doc?(code)
   end
 end
--- lib/seeing_is_believing/syntax_analyzer.rb
+++ lib/seeing_is_believing/syntax_analyzer.rb
@@ -12,45 +12,7 @@ class SeeingIsBelieving
       instance
     end
 
-    # We have to do this b/c Ripper sometimes calls on_tstring_end even when the string doesn't get ended
-    # e.g. SyntaxAnalyzer.new('"a').parse
-    def ends_match?(beginning, ending)
-      return false unless beginning && ending
-      return beginning == ending if beginning.size == 1 && ending.size == 1
-      case beginning[-1]
-      when '<' then '>' == ending
-      when '(' then ')' == ending
-      when '[' then ']' == ending
-      when '{' then '}' == ending
-      when '/' then ending =~ /\A\// # example: /a/x
-      else
-        # example: %Q.a. %_a_ %r|a| ...
-        beginning.start_with?('%') && beginning.end_with?(ending)
-      end
-    end
-
     # SYNTACTIC VALIDITY
-
-    # I don't actually know if all of the error methods should set @has_error
-    # or just parse errors. I don't actually know how to produce the other errors O.o
-    #
-    # Here is what it is defining as of ruby-1.9.3-p125:
-    #   on_alias_error
-    #   on_assign_error
-    #   on_class_name_error
-    #   on_param_error
-    #   on_parse_error
-    instance_methods.grep(/error/i).each do |error_meth|
-      super_meth = instance_method error_meth
-      define_method error_meth do |*args, &block|
-        @has_error = true
-        super_meth.bind(self).call(*args, &block)
-      end
-    end
-
-    def self.valid_ruby?(code)
-      parsed(code).valid_ruby? && begin_and_end_comments_are_complete?(code)
-    end
 
     def self.begins_multiline_comment?(line)
       line == '=begin'
@@ -66,70 +28,10 @@ class SeeingIsBelieving
           .all? { |b, e| b == '=begin' && e == '=end' }
     end
 
-    def valid_ruby?
-      !invalid_ruby?
-    end
-
-    def invalid_ruby?
-      @has_error || unclosed_string? || unclosed_regexp?
-    end
-
     # MISC
 
     def self.begins_data_segment?(line)
       line == '__END__'
-    end
-
-    def self.next_line_modifies_current?(line)
-      line =~ /^\s*\./
-    end
-
-    # STRINGS
-
-    def self.unclosed_string?(code)
-      parsed(code).unclosed_string?
-    end
-
-    def string_opens
-      @string_opens ||= []
-    end
-
-    def on_tstring_beg(beginning)
-      string_opens.push beginning
-      super
-    end
-
-    def on_tstring_end(ending)
-      string_opens.pop if ends_match? string_opens.last, ending
-      super
-    end
-
-    def unclosed_string?
-      string_opens.any?
-    end
-
-    # REGEXPS
-
-    def self.unclosed_regexp?(code)
-      parsed(code).unclosed_regexp?
-    end
-
-    def regexp_opens
-      @regexp_opens ||= []
-    end
-
-    def on_regexp_beg(beginning)
-      regexp_opens.push beginning
-      super
-    end
-
-    def on_regexp_end(ending)
-      regexp_opens.pop if ends_match? regexp_opens.last, ending
-      super
-    end
-
-    def unclosed_regexp?
-      regexp_opens.any?
     end
 
     # COMMENTS
@@ -154,63 +56,6 @@ class SeeingIsBelieving
     def on_comment(*)
       @has_comment = true
       super
-    end
-
-    # RETURNS
-
-    def self.void_value_expression?(code_or_ast)
-      ast = code_or_ast
-      ast = Parser::CurrentRuby.parse(code_or_ast) if code_or_ast.kind_of? String
-
-      case ast && ast.type
-      when :begin, :kwbegin, :resbody # begin and kwbegin should be the same thing, but it changed in parser 1.4.1 to 2.0.0, so just adding them both for safety
-        void_value_expression?(ast.children[-1])
-      when :rescue, :ensure
-        ast.children.any? { |child| void_value_expression? child }
-      when :if
-        void_value_expression?(ast.children[1]) || void_value_expression?(ast.children[2])
-      when :return, :next, :redo, :retry, :break
-        true
-      else
-        false
-      end
-    end
-
-    # HERE DOCS
-
-    def self.here_doc?(code)
-      instance = parsed code
-      instance.has_heredoc? && code.scan("\n").size.next <= instance.here_doc_last_line_number
-    end
-
-    def self.unfinished_here_doc?(code)
-      instance = parsed code
-      instance.heredocs.any? { |heredoc| heredoc.size == 1 }
-    end
-
-    def heredocs
-      @heredocs ||= []
-    end
-
-    def on_heredoc_beg(beginning)
-      heredocs << [beginning]
-      super
-    end
-
-    def on_heredoc_end(ending)
-      result      = super
-      line_number = result.last.first
-      doc = heredocs.find { |(beginning)| beginning.include? ending.strip }
-      doc << ending << line_number
-      result
-    end
-
-    def has_heredoc?
-      heredocs.any?
-    end
-
-    def here_doc_last_line_number
-      heredocs.last.last
     end
   end
 end
