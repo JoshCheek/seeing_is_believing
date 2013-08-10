d332079f2756a518b66107454fc103a674f580b5
Josh Cheek <josh.cheek@gmail.com>
2013-08-10 15:01:12 -0500

    Make a CommentFormatter based on LineFormatter, which we'll eventually delete

---
+++ lib/seeing_is_believing/binary/comment_formatter.rb
@@ -0,0 +1,54 @@
+class SeeingIsBelieving
+  class Binary
+    class CommentFormatter
+      def self.call(*args)
+        new(*args).call
+      end
+
+      attr_accessor :line, :separator, :result, :options
+
+      # we don't technically need the line for this, just its length, but w/e
+      def initialize(line, separator, result, options)
+       self.line      = line
+       self.separator = separator
+       self.result    = result.gsub "\n", '\n'
+       self.options   = options
+      end
+
+      def call
+        formatted = truncate "#{separator}#{result}", result_length
+        formatted = "#{' '*padding_length}#{formatted}"
+        formatted = truncate formatted, line_length
+        formatted = '' unless formatted.sub(/^ */, '').start_with? separator
+        formatted
+      end
+
+      private
+
+      def line_length
+        length = options.fetch(:line_length, Float::INFINITY) - line.size
+        length = 0 if length < 0
+        length
+      end
+
+      def result_length
+        options.fetch :result_length, Float::INFINITY
+      end
+
+      def padding_length
+        padding_length = options.fetch(:pad_to, 0) - line.size
+        padding_length = 0 if padding_length < 0
+        padding_length
+      end
+
+      def truncate(string, length)
+        return string if string.size <= length
+        ellipsify string.slice(0, length)
+      end
+
+      def ellipsify(string)
+        string.sub(/.{0,3}$/) { |last_chars| last_chars.gsub /./, '.' }
+      end
+    end
+  end
+end
---
+++ spec/binary/comment_formatter_spec.rb
@@ -0,0 +1,54 @@
+require 'seeing_is_believing/binary/comment_formatter'
+
+describe SeeingIsBelieving::Binary::CommentFormatter do
+  def result_for(line, separator, result, options={})
+    described_class.new(line, separator, result, options).call
+  end
+
+  specify 'it returns the consolidated result if there are no truncations' do
+    result_for('1', '=>', '12345').should == '=>12345'
+  end
+
+  specify 'result_length truncates a result to the specified length, using elipses up to that length if appropriate'  do
+    line      = '1'
+    separator = '=>'
+    result    = '12345'
+    result_for(line, separator, result, result_length: Float::INFINITY).should == '=>12345'
+    result_for(line, separator, result, result_length: 7).should == '=>12345'
+    result_for(line, separator, result, result_length: 6).should == '=>1...'
+    result_for(line, separator, result, result_length: 5).should == '=>...'
+    result_for(line, separator, result, result_length: 4).should == ''
+    result_for(line, separator, result, result_length: 0).should == ''
+  end
+
+  specify 'line_length truncates a result to the specified length, minus the length of the line' do
+    line      = '1'
+    separator = '=>'
+    result    = '12345'
+    result_for(line, separator, result).should == '=>12345'
+    result_for(line, separator, result, line_length: Float::INFINITY).should == '=>12345'
+    result_for(line, separator, result, line_length: 8).should == '=>12345'
+    result_for(line, separator, result, line_length: 7).should == '=>1...'
+    result_for(line, separator, result, line_length: 6).should == '=>...'
+    result_for(line, separator, result, line_length: 5).should == ''
+    result_for(line, separator, result, line_length: 0).should == ''
+  end
+
+  specify 'pad_to will pad the length that the line is displayed in',t:true do
+    result_for('1', '=>', '2', pad_to: 0).should == '=>2'
+    result_for('1', '=>', '2', pad_to: 1).should == '=>2'
+    result_for('1', '=>', '2', pad_to: 2).should == ' =>2'
+  end
+
+  specify 'pad_to is ignored when separator/result will not be printed' do
+    result_for('1', '=>', '12345', pad_to: 2,   line_length: 2).should == ''
+    result_for('1', '=>', '12345', pad_to: 2, result_length: 2).should == ''
+  end
+
+  specify 'they can all work together' do
+    result_for('1', '=>', '12345', line_length: 100, result_length: 100, pad_to: 2).should == ' =>12345'
+    result_for('1', '=>', '12345', line_length:   8, result_length: 100, pad_to: 2).should == ' =>1...'
+    result_for('1', '=>', '12345', line_length: 100, result_length:   6, pad_to: 2).should == ' =>1...'
+    result_for('1', '=>', '12345', line_length: 100, result_length:   6, pad_to: 2).should == ' =>1...'
+  end
+end
