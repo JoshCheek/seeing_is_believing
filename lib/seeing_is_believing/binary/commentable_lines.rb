902df3c2f0978696172cde5076c1f2d3b6ca7b34
Josh Cheek <josh.cheek@gmail.com>
2014-04-03 07:05:06 -0400

    Rename variable
    "indexes" implies integers
    "location" makes it clearer that they should go look at implementation to see specific structure

--- lib/seeing_is_believing/binary/commentable_lines.rb
+++ lib/seeing_is_believing/binary/commentable_lines.rb
@@ -20,12 +20,12 @@ class SeeingIsBelieving
 
       def call
         @call ||= begin
-          line_num_to_indexes = line_nums_to_last_index_and_col(buffer)
-          remove_lines_after_data_segment           line_num_to_indexes
-          remove_lines_whose_newline_is_escaped     line_num_to_indexes
-          remove_lines_ending_in_comments           line_num_to_indexes, comments
-          remove_lines_inside_of_strings_and_things line_num_to_indexes, root
-          line_num_to_indexes
+          line_num_to_location = line_nums_to_last_index_and_col(buffer)
+          remove_lines_after_data_segment           line_num_to_location
+          remove_lines_whose_newline_is_escaped     line_num_to_location
+          remove_lines_ending_in_comments           line_num_to_location, comments
+          remove_lines_inside_of_strings_and_things line_num_to_location, root
+          line_num_to_location
         end
       end
 
@@ -37,44 +37,44 @@ class SeeingIsBelieving
       attr_accessor :code, :parser, :root, :comments
 
       def line_nums_to_last_index_and_col(buffer)
-        line_num_to_indexes = code.each_char
-                                  .with_index
-                                  .select { |char, index| char == "\n" } # <-- is this okay? what about other OSes?
-                                  .each_with_object(Hash.new) do |(_, index), hash|
-                                    line, col = buffer.decompose_position index
-                                    hash[line] = [index, col]
-                                  end
+        line_num_to_location = code.each_char
+                                   .with_index
+                                   .select { |char, index| char == "\n" } # <-- is this okay? what about other OSes?
+                                   .each_with_object(Hash.new) do |(_, index), hash|
+                                     line, col = buffer.decompose_position index
+                                     hash[line] = [index, col]
+                                   end
         if code[code.size-1] != "\n" # account for the fact that the last line wouldn't have been found above if it doesn't end in a newline
           line, col = buffer.decompose_position code.size
-          line_num_to_indexes[line] = [code.size, col]
+          line_num_to_location[line] = [code.size, col]
         end
-        line_num_to_indexes
+        line_num_to_location
       end
 
-      def remove_lines_whose_newline_is_escaped(line_num_to_indexes)
-        line_num_to_indexes.select { |line_number, (index_of_newline, col)| code[index_of_newline-1] == '\\' }
-                           .each   { |line_number, (index_of_newline, col)| line_num_to_indexes.delete line_number }
+      def remove_lines_whose_newline_is_escaped(line_num_to_location)
+        line_num_to_location.select { |line_number, (index_of_newline, col)| code[index_of_newline-1] == '\\' }
+                            .each   { |line_number, (index_of_newline, col)| line_num_to_location.delete line_number }
       end
 
-      def remove_lines_ending_in_comments(line_num_to_indexes, comments)
+      def remove_lines_ending_in_comments(line_num_to_location, comments)
         comments.each do |comment|
           if comment.type == :inline
-            line_num_to_indexes.delete comment.location.line
+            line_num_to_location.delete comment.location.line
           else
             begin_pos = comment.location.expression.begin_pos
             end_pos   = comment.location.expression.end_pos
             range     = begin_pos...end_pos
-            line_num_to_indexes.select { |line_number, (index_of_newline, col)| range.include? index_of_newline }
-                               .each   { |line_number, (index_of_newline, col)| line_num_to_indexes.delete line_number }
+            line_num_to_location.select { |line_number, (index_of_newline, col)| range.include? index_of_newline }
+                                .each   { |line_number, (index_of_newline, col)| line_num_to_location.delete line_number }
           end
         end
       end
 
-      def remove_lines_inside_of_strings_and_things(line_num_to_indexes, ast)
+      def remove_lines_inside_of_strings_and_things(line_num_to_location, ast)
         invalid_boundaries = ranges_of_atomic_expressions ast, []
         invalid_boundaries.each do |invalid_boundary|
-          line_num_to_indexes.select { |line_number, (index_of_newline, col)| invalid_boundary.include? index_of_newline }
-                             .each   { |line_number, (index_of_newline, col)| line_num_to_indexes.delete line_number }
+          line_num_to_location.select { |line_number, (index_of_newline, col)| invalid_boundary.include? index_of_newline }
+                              .each   { |line_number, (index_of_newline, col)| line_num_to_location.delete line_number }
         end
       end
 
@@ -106,8 +106,8 @@ class SeeingIsBelieving
         end
       end
 
-      def remove_lines_after_data_segment(line_num_to_indexes)
-        data_segment_line, _ = line_num_to_indexes.find do |line_number, (end_index, col)|
+      def remove_lines_after_data_segment(line_num_to_location)
+        data_segment_line, _ = line_num_to_location.find do |line_number, (end_index, col)|
           if end_index == 7
             code.start_with? '__END__'
           elsif end_index < 7
@@ -117,8 +117,8 @@ class SeeingIsBelieving
           end
         end
         return unless data_segment_line
-        max_line = line_num_to_indexes.keys.max
-        data_segment_line.upto(max_line) { |line_number| line_num_to_indexes.delete line_number }
+        max_line = line_num_to_location.keys.max
+        data_segment_line.upto(max_line) { |line_number| line_num_to_location.delete line_number }
       end
     end
   end
