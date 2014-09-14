16b9ed1046b5807969210ad5b161d965d2bf262d
Josh Cheek <josh.cheek@gmail.com>
2014-09-14 01:11:40 -0600

    Beginning to write an event stream to replace the JSON dumping

---
+++ lib/seeing_is_believing/event_stream.rb
@@ -0,0 +1,115 @@
+class SeeingIsBelieving
+  # At the binary level, streaming will have to be opted into, b/c you'd need something on the other side that could display it
+  # TODO: we'll use eval for now, later just escape \ns
+  module EventStream
+    module Event
+      LineResult       = Struct.new(:type, :line_number, :inspected)
+      UnrecordedResult = Struct.new(:type, :line_number)
+    end
+
+    class Consumer
+      def initialize(readstream)
+        @readstream = readstream
+      end
+
+      def call(n=1)
+        return event_for @readstream.gets if n == 1
+        n.times.map { event_for @readstream.gets }
+      end
+
+      private
+
+      def extract_token(line)
+        event_name = line[/[^ ]*/]
+        line.sub! /[^ ]* /, ''
+        event_name
+      end
+
+      def extract_string(line)
+        puts "EXTRACTING: #{line.inspect}"
+        line
+      end
+
+      def event_for(line)
+        line.chomp!
+        event_name = extract_token(line)
+        case event_name
+        when 'result'
+          line_number = extract_token(line).to_i
+          type        = extract_token(line).intern
+          inspected   = extract_string(line)
+          Event::LineResult.new(type, line_number, inspected)
+        when 'maxed_result'
+          line_number = extract_token(line).to_i
+          type        = extract_token(line).intern
+          Event::UnrecordedResult.new(type, line_number)
+        else
+          raise "IDK what #{event_name.inspect} is!"
+        end
+      end
+    end
+
+    class Publisher
+      attr_accessor :exitstatus, :bug_in_sib, :max_line_captures  # => nil
+      attr_accessor :resultstream                                 # => nil
+      attr_accessor :recorded_results                             # => nil
+
+      def initialize(resultstream)
+        self.resultstream      = resultstream                                          # => #<StringIO:0x007f974404c788>
+        self.exitstatus        = 0                                                     # => 0
+        self.bug_in_sib        = false                                                 # => false
+        self.max_line_captures = Float::INFINITY                                       # => Infinity
+        self.recorded_results  = Hash.new { |h, line_num| h[line_num] = Hash.new(0) }  # => {}
+      end
+
+      # TODO: delete?
+      def bug_in_sib=(bool)
+        @bug_in_sib = !!bool  # => false
+      end
+
+      def safe_string(string)
+        puts "SAFE STRING: #{string.inspect}"
+        string.gsub! "\n", "\\n"
+        string
+      end
+
+      # TODO: can record basic object and that shit
+      # TODO: only records inspect once
+      # TODO: Check whatever else result is currently doing
+      def record_result(type, line_number, value)
+        count = recorded_results[line_number][type]
+        recorded_results[line_number][type] = count + 1
+        if count < max_line_captures
+          resultstream << "result #{line_number} #{type} #{safe_string value.inspect}\n"
+        elsif count == max_line_captures
+          resultstream << "maxed_result #{line_number} #{type}\n"
+        end
+        value
+      end
+
+      def record_exception(line_number, exception)
+        resultstream << "exception line_number #{line_number}\n"                        # => #<StringIO:0x007f974404c788>
+        resultstream << "exception class_name  #{exception.class.name.to_s.inspect}\n"  # => #<StringIO:0x007f974404c788>
+        resultstream << "exception message     #{exception.message.to_s.inspect}\n"     # => #<StringIO:0x007f974404c788>
+        exception.backtrace.each do |line|                                              # => ["/var/folders/7g/mbft22555w3_2nqs_h1kbglw0000gn/T/seeing_is_believing_temp_dir20140913-72389-i6ovhi/program.rb:66:in `<main>'"]
+          resultstream << "exception backtrace #{line.inspect}\n"                       # => #<StringIO:0x007f974404c788>
+        end                                                                             # => ["/var/folders/7g/mbft22555w3_2nqs_h1kbglw0000gn/T/seeing_is_believing_temp_dir20140913-72389-i6ovhi/program.rb:66:in `<main>'"]
+      end
+
+      # TODO with a mutex, we could also write this dynamically!
+      def record_stdout(stdout)
+        resultstream << "stdout #{stdout.inspect}\n"  # => #<StringIO:0x007f974404c788>
+      end
+
+      def record_stderr(stderr)
+        resultstream << "stderr #{stderr.inspect}\n"  # => #<StringIO:0x007f974404c788>
+      end
+
+      def finalize
+        resultstream << "bug_in_sib #{bug_in_sib}\n"                # => #<StringIO:0x007f974404c788>
+        resultstream << "max_line_captures #{max_line_captures}\n"  # => #<StringIO:0x007f974404c788>
+        resultstream << "exitstatus #{exitstatus}\n"                # => #<StringIO:0x007f974404c788>
+      end
+    end
+  end
+end
---
+++ spec/event_stream_spec.rb
@@ -0,0 +1,128 @@
+require 'seeing_is_believing/event_stream'
+
+RSpec.describe SeeingIsBelieving::EventStream do
+  LineResult       = SeeingIsBelieving::EventStream::Event::LineResult
+  UnrecordedResult = SeeingIsBelieving::EventStream::Event::UnrecordedResult
+
+  attr_accessor :publisher, :consumer, :readstream, :writestream
+
+  before do
+    self.readstream, self.writestream = IO.pipe
+    self.publisher = SeeingIsBelieving::EventStream::Publisher.new(writestream)
+    self.consumer  = SeeingIsBelieving::EventStream::Consumer.new(readstream)
+  end
+
+  after {
+    readstream.close  unless readstream.closed?
+    writestream.close unless writestream.closed?
+  }
+
+  describe 'emitting a result' do
+    it 'writes a line to stdout'
+    it 'is wrapped in a mutex to prevent multiple values from writing at the same time'
+  end
+
+  describe 'record_results' do
+    example 'Simple example' do
+      publisher.record_result :type, 1, "a"
+      expect(consumer.call).to eq LineResult.new(:type, 1, '"a"')
+
+      publisher.record_result :type, 1, 1
+      expect(consumer.call).to eq LineResult.new(:type, 1, '1')
+    end
+
+    it 'emits a type, line_number, and escaped string' do
+      publisher.record_result :type1, 123, [*'a'..'z', *'A'..'Z', *'0'..'9'].join("")
+      publisher.record_result :type1, 123, '"'
+      publisher.record_result :type1, 123, '""'
+      publisher.record_result :type1, 123, "\n"
+      publisher.record_result :type1, 123, "\r"
+      publisher.record_result :type1, 123, "\n\r\n"
+      publisher.record_result :type1, 123, "\#{}"
+      publisher.record_result :type1, 123, [*0..127].map(&:chr).join("")
+      publisher.record_result :type1, 123, "Ω≈ç√∫˜µ≤≥"
+
+      expect(consumer.call 9).to eq [
+        LineResult.new(:type1, 123, [*'a'..'z', *'A'..'Z', *'0'..'9'].join("").inspect),
+        LineResult.new(:type1, 123, '"'.inspect),
+        LineResult.new(:type1, 123, '""'.inspect),
+        LineResult.new(:type1, 123, "\n".inspect),
+        LineResult.new(:type1, 123, "\r".inspect),
+        LineResult.new(:type1, 123, "\n\r\n".inspect),
+        LineResult.new(:type1, 123, "\#{}".inspect),
+        LineResult.new(:type1, 123, [*0..127].map(&:chr).join("").inspect),
+        LineResult.new(:type1, 123, "Ω≈ç√∫˜µ≤≥".inspect),
+      ]
+    end
+
+    it 'indicates that there are more results once it hits the max, but does not continue reporting them' do
+      publisher.max_line_captures = 2
+
+      publisher.record_result :type1, 123, 1
+      expect(consumer.call 1).to eq LineResult.new(:type1, 123, '1')
+
+      publisher.record_result :type1, 123, 2
+      expect(consumer.call 1).to eq LineResult.new(:type1, 123, '2')
+
+      publisher.record_result :type1, 123, 3
+      publisher.record_result :type1, 123, 4
+      publisher.record_result :type2, 123, 1
+      expect(consumer.call 2).to eq [UnrecordedResult.new(:type1, 123),
+                                     LineResult.new(:type2, 123, '1')]
+    end
+
+    it 'scopes the max to a given type/line' do
+      publisher.max_line_captures = 1
+
+      publisher.record_result :type1, 1, 1
+      publisher.record_result :type1, 1, 2
+      publisher.record_result :type1, 2, 3
+      publisher.record_result :type1, 2, 4
+      publisher.record_result :type2, 1, 5
+      publisher.record_result :type2, 1, 6
+      expect(consumer.call 6).to eq [
+        LineResult.new(:type1, 1, '1'),
+        UnrecordedResult.new(:type1, 1),
+        LineResult.new(:type1, 2, '3'),
+        UnrecordedResult.new(:type1, 2),
+        LineResult.new(:type2, 1, '5'),
+        UnrecordedResult.new(:type2, 1),
+      ]
+    end
+
+    it 'returns the value' do
+      o = Object.new
+      expect(publisher.record_result :type, 123, o).to equal o
+    end
+  end
+
+  describe 'exceptions' do
+    it 'emits the line_number, an escaped class_name, an escaped message, and escaped backtrace'
+  end
+
+  describe 'stdout' do
+    it 'is an escaped string'
+  end
+
+  describe 'stderr' do
+    it 'is an escaped string'
+  end
+
+  describe 'finalize' do
+    describe 'bug_in_sib' do
+      it 'is true or false'
+      it 'is always emitted'
+    end
+
+    describe 'max_line_captures' do
+      it 'interprets numbers'
+      it 'interprets infinity'
+      it 'is infinity by default'
+    end
+
+    describe 'exitstatus' do
+      it 'is 0 by default'
+      it 'can be overridden'
+    end
+  end
+end
