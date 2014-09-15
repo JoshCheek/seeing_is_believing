JoshCheek/seeing_is_believing

commit 963f0fa329b2ebb591c53e326ba5d549e61d1f3d

EventStream waits for all elements to be pushed before finish! returns

lib/seeing_is_believing/event_stream.rb
@@ -192,6 +192,7 @@ class SeeingIsBelieving
         queue << "max_line_captures #{max_line_captures}"
         queue << "exitstatus #{exitstatus}"
         queue << "finish".freeze
+        publisher_thread.join
       end
 
       private

commit 005de4e2c1c3d59d92f7792bfdfb699bb4792d1d

Fix bug introduced in 963f0fa

lib/seeing_is_believing/event_stream.rb
@@ -131,15 +131,18 @@ class SeeingIsBelieving
         self.recorded_results  = []
         self.queue             = Thread::Queue.new
         self.publisher_thread  = Thread.new do
-          loop do
-            to_publish = queue.shift
-            File.open('published', 'a') { |f| f << "#{resultstream}\n" }
-            if to_publish == "finish".freeze
-              resultstream << "finish\n"
-              break
-            else
-              resultstream << (to_publish << "\n")
+          begin
+            loop do
+              to_publish = queue.shift
+              if to_publish == "finish".freeze
+                resultstream << "finish\n"
+                break
+              else
+                resultstream << (to_publish << "\n")
+              end
             end
+          rescue IOError, Errno::EPIPE
+            queue.clear # TODO: should probably keep emptying it until finish event
           end
         end
       end