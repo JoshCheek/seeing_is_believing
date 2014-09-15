4b9134ca45e001ebe5f139384bd1beee98b5e371
Josh Cheek <josh.cheek@gmail.com>
2014-09-14 23:42:17 -0600

    Reorganize the matrix, it was just so hard to read

--- lib/seeing_is_believing/the_matrix.rb
+++ lib/seeing_is_believing/the_matrix.rb
@@ -7,27 +7,27 @@
 
 require_relative 'event_stream'
 
-stdout_real_obj = STDOUT      # the real Ruby object, but its FD is going to keep getting reopened
-stderr_real_obj = STDERR
-stdout_real_fd  = STDOUT.dup  # duped Ruby object, but with the real file descriptor
-stderr_real_fd  = STDERR.dup
-
+stdout_real_obj = STDOUT      # the real Ruby object, fake file descriptor
+stdout_real_fd  = STDOUT.dup  # duped Ruby object,    real file descriptor
 read_from_mock_out, write_to_mock_out = IO.pipe
-read_from_mock_err, write_to_mock_err = IO.pipe
-
 stdout_real_obj.reopen write_to_mock_out
+
+stderr_real_obj = STDERR
+stderr_real_fd  = STDERR.dup
+read_from_mock_err, write_to_mock_err = IO.pipe
 stderr_real_obj.reopen write_to_mock_err
 
 $SiB = SeeingIsBelieving::EventStream::Publisher.new(stdout_real_fd)
 
 at_exit do
-  stdout_real_obj.reopen stdout_real_fd # TODO: Do I need to do this?
-  stderr_real_obj.reopen stderr_real_fd
+  stdout_real_obj.reopen stdout_real_fd
   write_to_mock_out.close unless write_to_mock_out.closed?
-  write_to_mock_err.close unless write_to_mock_err.closed?
   $SiB.record_stdout read_from_mock_out.read
-  $SiB.record_stderr read_from_mock_err.read
   read_from_mock_out.close
+
+  stderr_real_obj.reopen stderr_real_fd
+  write_to_mock_err.close unless write_to_mock_err.closed?
+  $SiB.record_stderr read_from_mock_err.read
   read_from_mock_err.close
 
   $SiB.exitstatus ||= 0
