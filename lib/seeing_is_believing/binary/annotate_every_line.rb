e50921757462fdacf95191571e697d68918035e1
Josh Cheek <josh.cheek@gmail.com>
2014-09-19 08:25:43 -0600

    Pull bulk of annotators's work up into call

--- lib/seeing_is_believing/binary/annotate_every_line.rb
+++ lib/seeing_is_believing/binary/annotate_every_line.rb
@@ -11,45 +11,38 @@ class SeeingIsBelieving
       end
 
       def initialize(body, results, options={})
-        self.options = options
-        self.body    = body
-        self.results = results
+        @options = options
+        @body    = body
+        @results = results
       end
 
       def call
         @new_body ||= begin
-          new_body = body_with_everything_annotated
+          require 'seeing_is_believing/binary/comment_lines'
+          require 'seeing_is_believing/binary/comment_formatter'
+          require 'seeing_is_believing/binary' # defines the markers
+
+          alignment_strategy = @options[:alignment_strategy].new(@body)
+          exception_lineno   = @results.has_exception? ? @results.exception.line_number : -1
+          new_body = CommentLines.call @body do |line, line_number|
+            options = @options.merge pad_to: alignment_strategy.line_length_for(line_number)
+            if exception_lineno == line_number
+              result = sprintf "%s: %s", @results.exception.class_name, @results.exception.message.gsub("\n", '\n')
+              CommentFormatter.call(line.size, EXCEPTION_MARKER, result, options)
+            elsif @results[line_number].any?
+              result  = @results[line_number].map { |result| result.gsub "\n", '\n' }.join(', ')
+              CommentFormatter.call(line.size, VALUE_MARKER, result, options)
+            else
+              ''
+            end
+          end
 
           require 'seeing_is_believing/binary/annotate_end_of_file'
-          AnnotateEndOfFile.add_stdout_stderr_and_exceptions_to new_body, results, options
+          AnnotateEndOfFile.add_stdout_stderr_and_exceptions_to new_body, @results, @options
 
           # What's w/ this debugger? maybe this should move higher?
-          options[:debugger].context "OUTPUT"
+          @options[:debugger].context "OUTPUT"
           new_body
-        end
-      end
-
-      private
-
-      attr_accessor :results, :body, :options, :alignment_strategy
-
-      def body_with_everything_annotated
-        require 'seeing_is_believing/binary/comment_lines'
-        require 'seeing_is_believing/binary/comment_formatter'
-        require 'seeing_is_believing/binary' # defines the markers
-        alignment_strategy = options[:alignment_strategy].new(body)
-        exception_lineno   = results.has_exception? ? results.exception.line_number : -1
-        CommentLines.call body do |line, line_number|
-          options = options().merge pad_to: alignment_strategy.line_length_for(line_number)
-          if exception_lineno == line_number
-            result = sprintf "%s: %s", results.exception.class_name, results.exception.message.gsub("\n", '\n')
-            CommentFormatter.call(line.size, EXCEPTION_MARKER, result, options)
-          elsif results[line_number].any?
-            result  = results[line_number].map { |result| result.gsub "\n", '\n' }.join(', ')
-            CommentFormatter.call(line.size, VALUE_MARKER, result, options)
-          else
-            ''
-          end
         end
       end
     end
--- lib/seeing_is_believing/binary/annotate_xmpfilter_style.rb
+++ lib/seeing_is_believing/binary/annotate_xmpfilter_style.rb
@@ -46,45 +46,36 @@ class SeeingIsBelieving
         }
       end
 
-      attr_accessor :results, :body
       def initialize(body, results, options={})
-        self.options = options
-        self.body    = body
-        self.results = results
+        @options = options
+        @body    = body
+        @results = results
       end
 
       def call
         @new_body ||= begin
-          new_body = body_with_updated_annotations
+          # TODO: doesn't currently realign output markers, do we want to do that?
+          require 'seeing_is_believing/binary' # defines the markers
+          require 'seeing_is_believing/binary/rewrite_comments'
+          require 'seeing_is_believing/binary/comment_formatter'
+          new_body = RewriteComments.call @body do |line_number, line_to_whitespace, whitespace, comment|
+            if !comment[VALUE_REGEX]
+              [whitespace, comment]
+            elsif line_to_whitespace.empty?
+              result = @results[line_number-1, :pp].map { |result| result.chomp }.join(', ') # TODO: CommentFormatter#initialize escapes newlines, need to be able to pass in that this shouldn't happen in this case
+              [whitespace, CommentFormatter.call(whitespace.size, VALUE_MARKER, result, @options)]
+            else
+              result = @results[line_number].map { |result| result.gsub "\n", '\n' }.join(', ')
+              [whitespace, CommentFormatter.call(line_to_whitespace.size + whitespace.size, VALUE_MARKER, result, @options)]
+            end
+          end
 
           require 'seeing_is_believing/binary/annotate_end_of_file'
-          AnnotateEndOfFile.add_stdout_stderr_and_exceptions_to new_body, results, options
+          AnnotateEndOfFile.add_stdout_stderr_and_exceptions_to new_body, @results, @options
 
           # What's w/ this debugger? maybe this should move higher?
-          options[:debugger].context "OUTPUT"
+          @options[:debugger].context "OUTPUT"
           new_body
-        end
-      end
-
-      private
-
-      attr_accessor :body, :options, :alignment_strategy
-
-      # doesn't currently realign output markers, do we want to do that?
-      def body_with_updated_annotations
-        require 'seeing_is_believing/binary' # defines the markers
-        require 'seeing_is_believing/binary/rewrite_comments'
-        require 'seeing_is_believing/binary/comment_formatter'
-        RewriteComments.call body do |line_number, line_to_whitespace, whitespace, comment|
-          if !comment[VALUE_REGEX]
-            [whitespace, comment]
-          elsif line_to_whitespace.empty?
-            result = results[line_number-1, :pp].map { |result| result.chomp }.join(', ') # TODO: CommentFormatter#initialize escapes newlines, need to be able to pass in that this shouldn't happen in this case
-            [whitespace, CommentFormatter.call(whitespace.size, VALUE_MARKER, result, options)]
-          else
-            result = results[line_number].map { |result| result.gsub "\n", '\n' }.join(', ')
-            [whitespace, CommentFormatter.call(line_to_whitespace.size + whitespace.size, VALUE_MARKER, result, options)]
-          end
         end
       end
     end
