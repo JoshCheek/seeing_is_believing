6588b65de601610997d761f8d073379e8942fbcf
Josh Cheek <josh.cheek@gmail.com>
2014-09-20 09:37:57 -0600

    Fix spelling error in private method

--- lib/seeing_is_believing/binary/remove_annotations.rb
+++ lib/seeing_is_believing/binary/remove_annotations.rb
@@ -37,7 +37,7 @@ class SeeingIsBelieving
           end
         end
 
-        remove_whitespace_preceeding_comments(buffer, rewriter, removed_comments)
+        remove_whitespace_preceding_comments(buffer, rewriter, removed_comments)
         rewriter.process
       rescue Parser::SyntaxError => e
         raise SyntaxError, e.message
@@ -47,7 +47,7 @@ class SeeingIsBelieving
 
       attr_accessor :code, :should_clean_values, :buffer
 
-      def remove_whitespace_preceeding_comments(buffer, rewriter, removed_comments)
+      def remove_whitespace_preceding_comments(buffer, rewriter, removed_comments)
         removed_comments[:result].each    { |comment| remove_whitespace_before comment.location.expression.begin_pos, buffer, rewriter, false }
         removed_comments[:exception].each { |comment| remove_whitespace_before comment.location.expression.begin_pos, buffer, rewriter, true  }
         removed_comments[:stdout].each    { |comment| remove_whitespace_before comment.location.expression.begin_pos, buffer, rewriter, true  }
@@ -55,14 +55,14 @@ class SeeingIsBelieving
       end
 
       # any whitespace before the index (on the same line) will be removed
-      # if the preceeding whitespace is at the beginning of the line, the newline will be removed
-      # if there is a newline before all of that, and remove_preceeding_newline is true, it will be removed as well
-      def remove_whitespace_before(index, buffer, rewriter, remove_preceeding_newline)
+      # if the preceding whitespace is at the beginning of the line, the newline will be removed
+      # if there is a newline before all of that, and remove_preceding_newline is true, it will be removed as well
+      def remove_whitespace_before(index, buffer, rewriter, remove_preceding_newline)
         end_pos   = index
         begin_pos = end_pos - 1
         begin_pos -= 1 while code[begin_pos] =~ /\s/ && code[begin_pos] != "\n"
         begin_pos -= 1 if code[begin_pos] == "\n"
-        begin_pos -= 1 if code[begin_pos] == "\n" && remove_preceeding_newline
+        begin_pos -= 1 if code[begin_pos] == "\n" && remove_preceding_newline
         return if begin_pos.next == end_pos
         rewriter.remove Parser::Source::Range.new(buffer, begin_pos.next, end_pos)
       end
