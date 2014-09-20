d272535832ccd910e4aa1ef47220c6448dae29a9
Josh Cheek <josh.cheek@gmail.com>
2014-09-20 09:39:02 -0600

    Xmpfilter style correctly prints pp style output
    Does not correctly remove it, yet, though.
    
    This is part of #44 & #47

--- lib/seeing_is_believing/binary/annotate_xmpfilter_style.rb
+++ lib/seeing_is_believing/binary/annotate_xmpfilter_style.rb
@@ -29,7 +29,7 @@ class SeeingIsBelieving
                                     should_inspect = inspect_linenos.include?(line_number)
                                     should_pp      = pp_linenos.include?(line_number)
                                     inspect        = "$SiB.record_result(:inspect, #{line_number}, v)"
-                                    pp             = "$SiB.record_result(:pp, #{line_number}, v) { PP.pp v, '', 74 }" # TODO: Is 74 the right value?
+                                    pp             = "$SiB.record_result(:pp, #{line_number}, v) { PP.pp v, '', 74 }" # TODO: Is 74 the right value? Prob not, I think it's 80(default width) - 1(comment width) - 5(" => {"), but if I allow indented `# => `, then that would need to be less than 74 (idk if I actually do this or not, though :P)
 
                                     if    should_inspect && should_pp then ").tap { |v| #{inspect}; #{pp} }"
                                     elsif should_inspect              then ").tap { |v| #{inspect} }"
@@ -46,21 +46,32 @@ class SeeingIsBelieving
         @results = results
       end
 
+      # TODO: I think that this should respect the alignment strategy
+      # and we should just add a new alignment strategy for default xmpfilter style
       def call
         @new_body ||= begin
           # TODO: doesn't currently realign output markers, do we want to do that?
           require 'seeing_is_believing/binary' # defines the markers
           require 'seeing_is_believing/binary/rewrite_comments'
           require 'seeing_is_believing/binary/comment_formatter'
-          new_body = RewriteComments.call @body do |line_number, line_to_whitespace, whitespace, comment|
+          new_body = RewriteComments.call @body do |line_number, preceding_code, whitespace, comment|
             if !comment[VALUE_REGEX]
               [whitespace, comment]
-            elsif line_to_whitespace.empty?
-              result = @results[line_number-1, :pp].map { |result| result.chomp }.join(', ') # TODO: CommentFormatter#initialize escapes newlines, need to be able to pass in that this shouldn't happen in this case
-              [whitespace, CommentFormatter.call(whitespace.size, VALUE_MARKER, result, @options)]
+            elsif preceding_code.empty?
+              # TODO: check that having multiple mult-line output values here looks good (e.g. avdi's example in a loop)
+              pp_value_marker = VALUE_MARKER.sub(/(?<=#).*$/) { |after_comment| ' ' * after_comment.size }
+              result          = @results[line_number-1, :pp].map { |result| result.chomp }.join(', ')
+              comment_lines   = result.each_line.map.with_index do |comment_line, result_offest|
+                if result_offest == 0
+                  CommentFormatter.call(preceding_code.size, VALUE_MARKER, comment_line.chomp, @options)
+                else
+                  CommentFormatter.call(preceding_code.size, pp_value_marker, comment_line.chomp, @options)
+                end
+              end
+              [whitespace, comment_lines.join("\n")]
             else
               result = @results[line_number].map { |result| result.gsub "\n", '\n' }.join(', ')
-              [whitespace, CommentFormatter.call(line_to_whitespace.size + whitespace.size, VALUE_MARKER, result, @options)]
+              [whitespace, CommentFormatter.call(preceding_code.size + whitespace.size, VALUE_MARKER, result, @options)]
             end
           end
 
