5dfc34a4fd95277ace73fcb94b5dfe1970048964
Josh Cheek <josh.cheek@gmail.com>
2014-09-23 01:36:20 -0600

    Fkn 2.0.0 -.-

--- spec/binary/interpret_flags_spec.rb
+++ spec/binary/interpret_flags_spec.rb
@@ -297,7 +297,7 @@ class SeeingIsBelieving
         inspected = call.inspect
         expect(inspected).to include "PREDICATES"
         expect(inspected).to include "ATTRIBUTES"
-        expect(inspected.lines.length).to be > 1
+        expect(inspected.lines.to_a.length).to be > 1
         inspected.lines.each do |line|
           expect(line.length).to be < 80 # truncate output so it doesn't get spammy
         end
