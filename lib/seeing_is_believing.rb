874d9e5f9a3ccc7f42e579b2883b93e6997d09aa
Josh Cheek <josh.cheek@gmail.com>
2015-01-08 05:59:35 -0700

    Syntactic cleanup

--- lib/seeing_is_believing.rb
+++ lib/seeing_is_believing.rb
@@ -16,15 +16,15 @@ class SeeingIsBelieving
 
   def initialize(program, options={})
     @program            = program
-    @filename           = options[:filename]
     @stdin              = to_stream options.fetch(:stdin, '')
-    @require            = options.fetch :require, ['seeing_is_believing/the_matrix']
-    @load_path          = options.fetch :load_path, []
-    @encoding           = options.fetch :encoding,  nil
-    @timeout            = options[:timeout]
-    @debugger           = options.fetch :debugger, Debugger.new(stream: nil)
+    @timeout            = options.fetch :timeout,            0
+    @load_path          = options.fetch :load_path,          []
+    @encoding           = options.fetch :encoding,           nil
+    @filename           = options.fetch :filename,           nil
+    @require            = options.fetch :require,            ['seeing_is_believing/the_matrix']
+    @debugger           = options.fetch :debugger,           Debugger.new(stream: nil)
     @number_of_captures = options.fetch :number_of_captures, Float::INFINITY
-    @evaluator          = options.fetch :evaluator, EvaluateByMovingFiles
+    @evaluator          = options.fetch :evaluator,          EvaluateByMovingFiles
     @record_expressions = options.fetch :record_expressions, InspectExpressions # TODO: rename to wrap_expressions
   end
 
