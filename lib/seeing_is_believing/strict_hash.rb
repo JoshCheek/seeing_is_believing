e521014950782422543f73363c32e56d68f9f52d
Josh Cheek <josh.cheek@gmail.com>
2015-01-12 08:06:43 -0700

    A start of StrictHash implementation

--- lib/seeing_is_believing/strict_hash.rb
+++ lib/seeing_is_believing/strict_hash.rb
@@ -1,5 +1,42 @@
 class SeeingIsBelieving
   class StrictHash
+    class << self
 
+      private
+
+      AttributeNotProvided = Module.new
+
+      def init_blocks
+        @init_blocks ||= {}
+      end
+
+      def attribute(name, value=AttributeNotProvided, &init_block)
+        value == AttributeNotProvided && !init_block and
+          raise ArgumentError, "Must provide a default value for #{name.inspect}"
+        init_blocks.key? name and
+          raise ArgumentError, "#{name} was already defined"
+        name.kind_of? Symbol or
+          raise ArgumentError, "#{name.inspect} should have been a symbol"
+
+        init_block ||= lambda { value }
+
+        init_blocks[name] = init_block
+        define_method(name, &init_block)
+        self
+      end
+
+      def attributes(pairs={})
+        pairs.each { |key, value| attribute key, value }
+        self
+      end
+
+      def predicate(name, *rest, &b)
+        attribute name, *rest, &b
+      end
+
+      def predicates(pairs)
+        attributes pairs
+      end
+    end
   end
 end
--- spec/strict_hash_spec.rb
+++ spec/strict_hash_spec.rb
@@ -1,20 +1,95 @@
 require 'seeing_is_believing/strict_hash'
 
 RSpec.describe SeeingIsBelieving::StrictHash do
+  let(:klass) {
+    klass = Class.new(described_class)
+    class << klass
+      public :attribute
+      public :attributes
+      public :predicate
+      public :predicates
+    end
+    klass
+  }
+
+  def eq!(expected, actual)
+    expect(expected).to eq actual
+  end
+
+  def raises!(*exception_class_and_matcher, &block)
+    expect(&block).to raise_error(*exception_class_and_matcher)
+  end
+
   describe 'declaration' do
     describe 'attributes' do
-      specify 'can be individually declared, requiring a default value or init block using .attribute'
-      specify 'can be group declared with a default value using .attributes(hash)'
+      specify 'can be individually declared, requiring a default value or init block using .attribute' do
+        eq! 1, klass.attribute(:a,   1 ).new.a
+        eq! 2, klass.attribute(:b) { 2 }.new.b
+        raises!(ArgumentError) { klass.attribute :c }
+      end
+
+      specify 'the block form is always called' do
+        klass.attribute(:a,   "a" ).new.a << "-modified"
+        eq! "a-modified", klass.new.a
+
+        klass.attribute(:b) { "b" }.new.b << "-modified"
+        eq! "b",          klass.new.b
+      end
+
+      specify 'can be group declared with a default value using .attributes(hash)' do
+        klass.attributes a: 1, b: 2
+        eq! 1, klass.new.a
+        eq! 2, klass.new.b
+      end
     end
 
-    describe 'predicates' do
-      specify 'are attributes'
-      specify 'can be individually declared with .predicate'
-      specify 'can be group declared with .predicates'
+    describe 'predicates are attributes which' do
+      specify 'can be individually declared with .predicate' do
+        eq! 1, klass.predicate(:a,   1 ).new.a
+        eq! 2, klass.predicate(:b) { 2 }.new.b
+        raises!(ArgumentError) { klass.predicate :c }
+      end
+      specify 'can be group declared with .predicates' do
+        klass.predicates a: 1, b: 2
+        eq! 1, klass.new.a
+        eq! 2, klass.new.b
+      end
     end
 
     describe 'conflicts' do
-      it 'raises if you double-declare an attribute'
+      it 'raises if you double-declare an attribute' do
+        klass.attribute :a, 1
+        raises!(ArgumentError) { klass.attribute :a, 2 }
+        raises!(ArgumentError) { klass.predicate :a, 3 }
+        raises!(ArgumentError) { klass.attributes a: 4 }
+        raises!(ArgumentError) { klass.predicates a: 5 }
+        eq! 1, klass.new.a
+
+        klass.predicate :b, 1
+        raises!(ArgumentError) { klass.attribute :b, 2 }
+        raises!(ArgumentError) { klass.predicate :b, 3 }
+        raises!(ArgumentError) { klass.attributes b: 4 }
+        raises!(ArgumentError) { klass.predicates b: 5 }
+        eq! 1, klass.new.b
+      end
+    end
+
+    describe '.attribute / .attributes / .predicate / .predicates' do
+      specify 'are private' do
+        raises!(NoMethodError) { Class.new(described_class).attribute :a, 1 }
+        eq! 1, Class.new(described_class) { attribute :a, 1 }.new.a
+      end
+
+      specify 'raise if a key is not a symbol (you shouldn\'t be dynamically creating this class with strings)' do
+        raises!(ArgumentError) { klass.attribute 'a', 1 }
+        raises!(ArgumentError) { klass.predicate 'b', 1 }
+        raises!(ArgumentError) { klass.attributes 'c' => 1 }
+        raises!(ArgumentError) { klass.predicates 'd' => 1 }
+      end
+
+      specify 'accept nil as a value (common edge case)' do
+        eq! nil, klass.attribute(:a, nil).new.a
+      end
     end
   end
 
@@ -53,13 +128,17 @@ RSpec.describe SeeingIsBelieving::StrictHash do
     end
 
     describe 'enumerability' do
-      it 'is enumerable, iterating over each attribute/value pair'
+      it 'is enumerable, iterating over each attribute(as symbol)/value pair'
       it 'iterates in the order the attributes were declared'
     end
 
     describe 'keys/values' do
       specify 'keys returns an array of symbols of all its attributes'
       specify 'values returns an array of symbol values'
+    end
+
+    describe '#key? / #has_key? / #include? / #member?' do
+      specify 'return true iff the key (symbolic or string) is an attribute'
     end
   end
 end
