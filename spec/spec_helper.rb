3ee8e1976cc1b24e3a154278d12443b7ae2649cf
Josh Cheek <josh.cheek@gmail.com>
2015-01-14 03:06:43 -0700

    A can use pending! in test suite to not have to raise afterwards
    Which is apparently like 80% of my use cases.
    Frankly, I liked the old behaviour where you used a block if you wanted it to fail when it passed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+module SibSpecHelpers
+  def pending!(message="Not yet implemented")
+    pending message
+    raise message
+  end
+end
+
 RSpec.configure do |c|
   c.disable_monkey_patching!
+  c.include SibSpecHelpers
 end
