874aec9e228f3280b15a1e04b7082efebb72cb24
Josh Cheek <josh.cheek@gmail.com>
2016-03-21 03:04:32 -0500

    Fix bug where some syntax would explode, and where Ruby 2.3 was causing a syntax error on mrspec's default output

--- lib/seeing_is_believing/wrap_expressions_with_inspect.rb
+++ lib/seeing_is_believing/wrap_expressions_with_inspect.rb
@@ -2,12 +2,14 @@ require 'seeing_is_believing/wrap_expressions'
 class SeeingIsBelieving
   module WrapExpressionsWithInspect
     def self.call(program)
+      # NOTE: if it received the AST, it could figure out if it needs
+      # to always wrap the expression in parentheses
       WrapExpressions.call program,
         before_each: -> line_number {
-          "$SiB.record_result(:inspect, #{line_number}, "
+          "$SiB.record_result(:inspect, #{line_number}, ("
         },
         after_each:  -> line_number {
-          ")"
+          "))"
         }
     end
   end
